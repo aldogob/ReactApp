20fa3b9c6a1109fe16bf3a81477b160202b948dc
unknown <you@example.com>
2021-02-11 12:53:51 -0800

    React

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -4,17 +4,15 @@ import YTsearch from 'youtube-api-search';
 import SearchBar from './componets/searchbar';
 const API_KEY = 'AIzaSyBNlfM2INlVMyvYjuNflpsfcx9FVM7E1hc';
 
-// create a new component. this componet
-//should produce some html
 class App extends Component{
     constructor(props) {
         super(props);
 
         this.state = { videos:[] };
 
-        YTsearch({key: API_KEY, term: 'surfboards'}, function(data) {
-            this.setState({ videos: data });
-
+        YTsearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos });
+        });
     }
 
     render() {
@@ -26,6 +24,4 @@ class App extends Component{
  }
 }
 
-//take this componets generated HTML 
-//put it on page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
