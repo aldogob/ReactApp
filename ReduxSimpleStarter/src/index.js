ab39a150706b9470c48161f8ca33971835cffac8
unknown <you@example.com>
2021-02-15 13:17:58 -0800

    React app- vidoes from youtube on page

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,14 +1,15 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
 import YTsearch from 'youtube-api-search';
-import SearchBar from './componets/searchbar';
+import SearchBar from './componets/search_bar';
+import VideoList from './componets/video_list';
 const API_KEY = 'AIzaSyBNlfM2INlVMyvYjuNflpsfcx9FVM7E1hc';
 
 class App extends Component{
     constructor(props) {
         super(props);
 
-        this.state = { videos:[] };
+        this.state = { videos: [] };
 
         YTsearch({key: API_KEY, term: 'surfboards'}, (videos) => {
             this.setState({ videos });
@@ -16,12 +17,13 @@ class App extends Component{
     }
 
     render() {
-    return (
+     return (
      <div>
-            <SearchBar />
-        </div>
-    );
- }
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+     </div>
+     );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
